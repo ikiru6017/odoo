9ca110f7133649a5931cc9adfd1bc5417d2bd8fb
antonyakimchuk <anton.yakimchuk7@gmail.com>
2019-09-27 21:18:12 +0300

    update

--- addons/mymod/models/models.py
+++ addons/mymod/models/models.py
@@ -2,14 +2,15 @@
 
 from odoo import models, fields, api
 
-# class mymod(models.Model):
-#     _name = 'mymod.mymod'
+class students(models.Model):
+    _name = 'ai.table'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    value3 = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
