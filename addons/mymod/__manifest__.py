945521866cd9cdcac98c9102c334f48af86879d8
antonyakimchuk <anton.yakimchuk7@gmail.com>
2019-09-27 20:18:34 +0300

    add information

--- addons/mymod/__manifest__.py
+++ addons/mymod/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "mymod",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        course work""",
 
     'description': """
         Long description of module's purpose
@@ -16,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'edu',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
