629b69844b7c2d48e43b99af758d253f1a494dfb
antonyakimchuk <anton.yakimchuk7@gmail.com>
2019-10-11 21:33:27 +0300

    ups

--- addons/mymod/models/models.py
+++ addons/mymod/models/models.py
@@ -10,6 +10,21 @@ class students(models.Model):
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     value3 = fields.Text()
+    group_id = fields.Many2one(comodel_name="mymod.groups")
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
+
+
+class groups(models.Model):
+    _name = 'mymod.groups'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+    students_ids = fields.One2many(comodel_name="ai.table" , inverse_name="group_id")
 
     @api.depends('value')
     def _value_pc(self):
